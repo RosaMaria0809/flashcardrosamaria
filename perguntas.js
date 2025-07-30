6e56fc08dfe2e8c5ca4376cc07ccfdb94ec6d971
RosaMaria0809 <rosa.souza.santos@escola.pr.gov.br>
2025-07-30 12:25:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'curiosidade sobre polvos ',
+    'O polvo tem três corações!',
+    'Dois corações bombeiam sangue para as brânqueas e para o corpo.Quando ele nada, o coração principal para de bater'
+)
+
+criaCartao(
+    'Curiosidade sobre formiga',
+    'As formigas não dormem',
+    'Elas entram em um curto período de descanso,mas nunca dormemde verade.Mesmo assim, conseguem manter a energia e coordenação'
+)
+
+criaCartao(
+    'curiosidade sobre camarões',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
